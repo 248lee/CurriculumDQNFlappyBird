fc107088a5dbf1adf56381a11caa9ccca8b0511b
johnlee <fantasy10final@gmail.com>
2023-12-23 01:03:55 +0800

    Ready for second stage

--- non_gui.py
+++ non_gui.py
@@ -1,2 +1,2 @@
 from deep_q_network import trainNetwork
-trainNetwork(1, 3, lock_mode=2, is_simple_actions_locked=False, max_steps=500000, resume_Adam=False, learning_rate=1e-5, event=None, is_colab=True)
+trainNetwork(2, 3, lock_mode=2, is_simple_actions_locked=False, max_steps=500000, resume_Adam=False, learning_rate=1e-5, event=None, is_colab=True)
