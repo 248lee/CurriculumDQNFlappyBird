56065df30bb1ba22bdc061dd9d9185e56fbf4bbf
johnlee <fantasy10final@gmail.com>
2023-12-26 09:16:30 +0800

    Training: Not Bad

--- non_gui2.py
+++ non_gui2.py
@@ -1,2 +1,2 @@
 from deep_q_network import trainNetwork
-trainNetwork(1, 3, lock_mode=1, is_simple_actions_locked=True, max_steps=300000, resume_Adam=False, learning_rate=1e-6, event=None, is_colab=True)
+trainNetwork(2, 3, lock_mode=2, is_simple_actions_locked=False, max_steps=400000, resume_Adam=False, learning_rate=1e-7, event=None, is_colab=True)
